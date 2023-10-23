014ebd51ba9ee3abc9ae6acaa44ad254646eafb5
Antonio P <106171750+Letualtv@users.noreply.github.com>
2023-10-23 19:29:36 +0200

    update

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { images } from '../../constants';
 
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="letra g" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">Nuestro Equipo</h1>
+        <img src={images.spoon} alt="cuchara sobre nosotros" className='spoon__img' />
+        <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non voluptates nam qui velit consequatur a nobis magnam sapiente soluta reprehenderit delectus excepturi fugit, dolore voluptatum similique. Saepe repudiandae harum dolorum.</p>
+        <button type='button' className='custom__button'>Saber más</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="cuchillo" />
+      </div>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Nuestra Historia</h1>
+        <img src={images.spoon} alt="cuchara sobre nosotros" className='spoon__img' />
+        <p className="p__opensans">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum dolore iusto culpa quo aliquam maiores tenetur repudiandae minus vero animi commodi, corporis temporibus error voluptate autem velit voluptatum eaque necessitatibus!</p>
+        <button type='button' className='custom__button'>Saber más</button>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { images } from '../../constants'
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 
 import './Header.css';
@@ -7,12 +8,14 @@ import './Header.css';
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className="app__wrapper_info">
-
-
+<SubHeading title="descubre el sabor" />
+<h1 className="app__header-h1">La Clave de una Buena Cena</h1>
+<p className="p__opensans" style={{ margin: '2rem 0'}}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum nihil officiis perspiciatis quo, non exercitationem asperiores. Saepe, et minus.</p>
+<button type="button" className="custom__button">Descubrir Menú</button>
     </div>
     
     <div className="app__wrapper_img">
-
+<img src={images.welcome} alt="imagen de comida" />
     </div>
   </div>
 );
